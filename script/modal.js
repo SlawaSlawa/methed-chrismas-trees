2badbe93a14df9f25335dc23963a043be415245e
Slawa <slawapocht4@yandex.ru>
2021-12-24 11:55:35 +0400

    day four: home-work added consultation-modal

--- script/modal.js
+++ script/modal.js
@@ -1,6 +1,10 @@
 const buttonsOrder = document.querySelectorAll('.product_order')
+const buttonConsultation = document.querySelector('.header__consultation')
+
 const overlayOrder = document.querySelector('.overlay_order')
 const order = overlayOrder.querySelector('.modal__order')
+
+const overlayConsultation = document.querySelector('.overlay_consultation')
 
 buttonsOrder.forEach(buttonOrder => {
     buttonOrder.addEventListener('click', () => {
@@ -14,5 +18,17 @@ overlayOrder.addEventListener('click', (evt) => {
 
     if (target === overlayOrder || target.closest('.modal__close')) {
         overlayOrder.classList.remove('overlay_active')
+    }
+})
+
+buttonConsultation.addEventListener('click', () => {
+    overlayConsultation.classList.add('overlay_active')
+})
+
+overlayConsultation.addEventListener('click', (evt) => {
+    const target = evt.target
+
+    if (target === overlayConsultation || target.closest('.modal__close')) {
+        overlayConsultation.classList.remove('overlay_active')
     }
 })
